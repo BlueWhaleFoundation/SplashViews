7bd615e0dfaed969590eeddcf042bf02eae31930
Sungjun Hong <>
2019-02-21 15:51:21 +0900

    Add custom colors to PasswordDialog and PasswordView Release 40

--- splashviews/src/main/java/foundation/bluewhale/splashviews/model/PasswordViewColors.kt
+++ splashviews/src/main/java/foundation/bluewhale/splashviews/model/PasswordViewColors.kt
@@ -1,7 +1,7 @@
 package foundation.bluewhale.splashviews.model
 
 data class PasswordViewColors (
-    , val pwTextColor: Int
+    val pwTextColor: Int
     , val pwErrorTextColor: Int
     , val pwCircleColor: Int
     , val pwUnderlineColor: Int
