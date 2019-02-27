b7e51283fe87a270266f651fc7f0f7c7fbc6dec0
Sungjun Hong <>
2019-02-27 13:14:28 +0900

    Release 66

--- splashviews/src/main/java/foundation/bluewhale/splashviews/network/BaseNetErrorHandler.java
+++ splashviews/src/main/java/foundation/bluewhale/splashviews/network/BaseNetErrorHandler.java
@@ -3,62 +3,6 @@ package foundation.bluewhale.splashviews.network;
 import foundation.bluewhale.splashviews.R;
 
 public class BaseNetErrorHandler {
-    public static class ServerErrorState{
-        boolean isProductionVersion;
-        String errorMessage = null;
-        int stringRes = 0;
-        public ServerErrorState(){}
-
-        public ServerErrorState(int stringRes) {
-            this.stringRes = stringRes;
-            errorMessage = "NO_INTERNET";
-        }
-
-        public ServerErrorState(boolean isProductionVersion, String errorMessage) {
-            this.errorMessage = errorMessage;
-            stringRes = makeErrorMessage(isProductionVersion, errorMessage);
-        }
-
-        public String getErrorMessage() {
-            return errorMessage;
-        }
-
-        public int getStringRes() {
-            return stringRes;
-        }
-
-        public boolean isObsoleteVersion(){
-            return "DEPRECATED_VERSION".equals(errorMessage);
-        }
-
-        public boolean isNotAuthorizedUser(){
-            return "AUTH.NOT_AUTHORIZED".equals(errorMessage)
-                    || "AUTH.VALIDATION_OUTDATED".equals(errorMessage)
-                    || "USER.INVALID_USER".equals(errorMessage);
-        }
-        public boolean isInvalidPassword(){
-            return "AUTH.INVALID_PASSWORD".equals(errorMessage);
-        }
-        public boolean hasInsufficientMoney(){
-            return "USER.NOT_ENOUGH_MONEY".equals(errorMessage);
-        }
-
-        public boolean isExceededMaxPwdFail(){
-            return "AUTH.EXCEED_MAX_PWD_FAIL".equals(errorMessage);
-        }
-
-        public boolean isNoUser(){
-            return "USER.NO_USER".equals(errorMessage);
-        }
-
-        public boolean cannotPurchase(){
-            return "TICKET.NOT_ENOUGH_TICKETS".equals(errorMessage)
-                    || "TICKET.ALREADY_BOUGHT".equals(errorMessage)
-                    || "TICKET.ALREADY_REFUNDED_OR_UESD".equals(errorMessage)
-                    || "ORDER.EXPIRED_ORDER".equals(errorMessage);
-        }
-    }
-
     public static int makeErrorMessage(boolean isProductionVersion, String message) {
         switch (message) {
             case "USER.NO_USER":
