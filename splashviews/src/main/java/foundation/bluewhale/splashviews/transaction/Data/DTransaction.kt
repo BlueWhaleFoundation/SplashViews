ffa07a9f925814eaf41b0dc460cda797a206a3ee
meuus90 <meuus90@bluewhale.foundation>
2019-03-12 17:28:54 +0900

    Add enum class Status on DTransaction

--- splashviews/src/main/java/foundation/bluewhale/splashviews/transaction/Data/DTransaction.kt
+++ splashviews/src/main/java/foundation/bluewhale/splashviews/transaction/Data/DTransaction.kt
@@ -6,60 +6,70 @@ import kotlinx.android.parcel.Parcelize
 import java.math.BigDecimal
 
 @Parcelize
-data class DTransaction constructor(val _id: String?
-                                    , val transactionId: String?
-                                    , val status: String?
-                                    , val amount: BigDecimal?
-                                    , val channel: String?
-                                    , val method: String?
-                                    , val type: String?
-                                    , val from: String?
-                                    , val fromInfo: UserInfo?
-                                    , val to: String?
-                                    , val toInfo: UserInfo?
-                                    , val currency: String?
-                                    , val orderInfo: OrderInfo?
-                                    , val paymentInfo: PaymentInfo?
-                                    , val bonusInfo: BonusInfo?
-                                    , val rewardInfo: RewardInfo?
-                                    , val withdrawInfo: WithdrawInfo?
-                                    , val createdAt: Long?
-                                    , val updatedAt: Long?
-                                    , val memo: String?
+data class DTransaction constructor(
+    val _id: String?
+    , val transactionId: String?
+    , val status: String?
+    , val amount: BigDecimal?
+    , val channel: String?
+    , val method: String?
+    , val type: String?
+    , val from: String?
+    , val fromInfo: UserInfo?
+    , val to: String?
+    , val toInfo: UserInfo?
+    , val currency: String?
+    , val orderInfo: OrderInfo?
+    , val paymentInfo: PaymentInfo?
+    , val bonusInfo: BonusInfo?
+    , val rewardInfo: RewardInfo?
+    , val withdrawInfo: WithdrawInfo?
+    , val createdAt: Long?
+    , val updatedAt: Long?
+    , val memo: String?
 ) : Parcelable {
     @Parcelize
-    data class UserInfo constructor(val name: String?
-                                    , val email: String?
-                                    , val tel: String?
-                                    , val leftBp: BigDecimal?
+    data class UserInfo constructor(
+        val name: String?
+        , val email: String?
+        , val tel: String?
+        , val leftBp: BigDecimal?
+        , val status: Status?
     ) : Parcelable
 
     @Parcelize
-    data class PaymentInfo constructor(val thirdPartyId: String?
-                                       , val business: String?
-                                       , val storeName: String?
+    data class PaymentInfo constructor(
+        val thirdPartyId: String?
+        , val business: String?
+        , val storeName: String?
     ) : Parcelable
 
     @Parcelize
-    data class BonusInfo constructor(val business: String?
-                                     , val storeName: String?
+    data class BonusInfo constructor(
+        val business: String?
+        , val storeName: String?
     ) : Parcelable
 
     @Parcelize
-    data class RewardInfo constructor(val rewardRatio: Double?
+    data class RewardInfo constructor(
+        val rewardRatio: Double?
     ) : Parcelable
 
     @Parcelize
-    data class WithdrawInfo constructor(val bankName: String?
-                                        , val bankAccount: String?
+    data class WithdrawInfo constructor(
+        val bankName: String?
+        , val bankAccount: String?
     ) : Parcelable
 
     @Parcelize
-    data class OrderInfo constructor(val businessId: String?
-                                     , val storeName: String?
+    data class OrderInfo constructor(
+        val businessId: String?
+        , val storeName: String?
     ) : Parcelable
 
 
     @IgnoredOnParcel
-    var headerId:Long = 0
+    var headerId: Long = 0
+
+    enum class Status { ACTIVATED, REMOVED }
 }
