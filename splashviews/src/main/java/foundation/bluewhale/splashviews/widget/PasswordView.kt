3e8a581dc0870a501a7f1ccc47ac3e119aa1a5c1
meuus90 <meuus90@bluewhale.foundation>
2019-03-19 17:36:37 +0900

    Add animation on password dialog

--- splashviews/src/main/java/foundation/bluewhale/splashviews/widget/PasswordView.kt
+++ splashviews/src/main/java/foundation/bluewhale/splashviews/widget/PasswordView.kt
@@ -1,53 +1,64 @@
 package foundation.bluewhale.splashviews.widget
 
 import android.content.Context
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.jakewharton.rxbinding2.view.RxView
-import foundation.bluewhale.splashviews.R
+import foundation.bluewhale.splashviews.R.*
 import foundation.bluewhale.splashviews.model.PasswordViewColors
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableObserver
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.widget_password_view.view.*
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
+
 
 class PasswordView : ConstraintLayout {
-
-
-    internal var pwTextColor: Int = 0
-    internal var pwErrorTextColor: Int = 0
-    internal var pwCircleColor: Int = 0
-    internal var pwUnderlineColor: Int = 0
+    private var pwTextColor: Int = 0
+    private var pwErrorTextColor: Int = 0
+    private var pwCircleColor: Int = 0
+    private var pwUnderlineColor: Int = 0
+    private val pwdAnim: ArrayList<Animation> = ArrayList(6)
+    private val pwdLineAnim: ArrayList<Animation> = ArrayList(6)
 
     constructor(context: Context) : super(context, null) {
+        for (i in 0..5) {
+            pwdAnim.add(AnimationUtils.loadAnimation(context, anim.pwd_add_char))
+            pwdLineAnim.add(AnimationUtils.loadAnimation(context, anim.pwd_line_add_char))
+        }
         initView(context)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.PasswordView)
+        val ta = context.obtainStyledAttributes(attrs, styleable.PasswordView)
         if (ta != null) {
             pwTextColor =
-                ta.getColor(R.styleable.PasswordView_pwTextColor, ContextCompat.getColor(context, R.color.pwTextColor))
+                ta.getColor(styleable.PasswordView_pwTextColor, ContextCompat.getColor(context, color.pwTextColor))
             pwErrorTextColor = ta.getColor(
-                R.styleable.PasswordView_pwErrorTextColor,
-                ContextCompat.getColor(context, R.color.pwErrorTextColor)
+                styleable.PasswordView_pwErrorTextColor,
+                ContextCompat.getColor(context, color.pwErrorTextColor)
             )
             pwCircleColor = ta.getColor(
-                R.styleable.PasswordView_pwCircleColor,
-                ContextCompat.getColor(context, R.color.pwCircleColor)
+                styleable.PasswordView_pwCircleColor,
+                ContextCompat.getColor(context, color.pwCircleColor)
             )
             pwUnderlineColor = ta.getColor(
-                R.styleable.PasswordView_pwUnderlineColor,
-                ContextCompat.getColor(context, R.color.pwUnderlineColor)
+                styleable.PasswordView_pwUnderlineColor,
+                ContextCompat.getColor(context, color.pwUnderlineColor)
             )
+        }
+        for (i in 0..5) {
+            pwdAnim.add(AnimationUtils.loadAnimation(context, anim.pwd_add_char))
+            pwdLineAnim.add(AnimationUtils.loadAnimation(context, anim.pwd_line_add_char))
         }
         initView(context)
     }
@@ -69,16 +80,46 @@ class PasswordView : ConstraintLayout {
         super.onDetachedFromWindow()
     }
 
-    internal var callback_completed: PublishSubject<Int>? = null
-    protected var _disposables: CompositeDisposable? = null
-    internal var keyArray = ArrayList<Int>()
+    private var callback_completed: PublishSubject<Int>? = null
+    private var _disposables: CompositeDisposable? = null
+    private var keyArray = ArrayList<Int>()
+
+    private val keypadList: ArrayList<TextView> = ArrayList(11)
+    private val pwdList: ArrayList<View> = ArrayList(6)
+    private val lineList: ArrayList<View> = ArrayList(6)
 
     internal fun initView(context: Context) {
         _disposables = CompositeDisposable()
 
         val li = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = li.inflate(R.layout.widget_password_view, this, false)
+        val view = li.inflate(layout.widget_password_view, this, false)
         addView(view)
+
+        keypadList.add(keypad_0)
+        keypadList.add(keypad_1)
+        keypadList.add(keypad_2)
+        keypadList.add(keypad_3)
+        keypadList.add(keypad_4)
+        keypadList.add(keypad_5)
+        keypadList.add(keypad_6)
+        keypadList.add(keypad_7)
+        keypadList.add(keypad_8)
+        keypadList.add(keypad_9)
+        keypadList.add(keypad_delete_all)
+
+        pwdList.add(v_pwd_0)
+        pwdList.add(v_pwd_1)
+        pwdList.add(v_pwd_2)
+        pwdList.add(v_pwd_3)
+        pwdList.add(v_pwd_4)
+        pwdList.add(v_pwd_5)
+
+        lineList.add(v_line_0)
+        lineList.add(v_line_1)
+        lineList.add(v_line_2)
+        lineList.add(v_line_3)
+        lineList.add(v_line_4)
+        lineList.add(v_line_5)
 
         refreshColors()
 
@@ -102,64 +143,32 @@ class PasswordView : ConstraintLayout {
                 })
         )
 
-        val clickListener = OnClickListener { v ->
-            val i = v.id
-            if (i == R.id.keypad_0) {
-                addNumber(keyArray[0])
-
-            } else if (i == R.id.keypad_1) {
-                addNumber(keyArray[1])
-
-            } else if (i == R.id.keypad_2) {
-                addNumber(keyArray[2])
-
-            } else if (i == R.id.keypad_3) {
-                addNumber(keyArray[3])
-
-            } else if (i == R.id.keypad_4) {
-                addNumber(keyArray[4])
-
-            } else if (i == R.id.keypad_5) {
-                addNumber(keyArray[5])
-
-            } else if (i == R.id.keypad_6) {
-                addNumber(keyArray[6])
-
-            } else if (i == R.id.keypad_7) {
-                addNumber(keyArray[7])
-
-            } else if (i == R.id.keypad_8) {
-                addNumber(keyArray[8])
-
-            } else if (i == R.id.keypad_9) {
-                addNumber(keyArray[9])
-
-            } else if (i == R.id.keypad_delete_all) {
-                deleteAll()
-
-            } else if (i == R.id.keypad_delete_one) {
-                deleteOne()
-
+        val clickListener = OnClickListener {
+            when (it.id) {
+                foundation.bluewhale.splashviews.R.id.keypad_0 -> addNumber(keyArray[0])
+                foundation.bluewhale.splashviews.R.id.keypad_1 -> addNumber(keyArray[1])
+                foundation.bluewhale.splashviews.R.id.keypad_2 -> addNumber(keyArray[2])
+                foundation.bluewhale.splashviews.R.id.keypad_3 -> addNumber(keyArray[3])
+                foundation.bluewhale.splashviews.R.id.keypad_4 -> addNumber(keyArray[4])
+                foundation.bluewhale.splashviews.R.id.keypad_5 -> addNumber(keyArray[5])
+                foundation.bluewhale.splashviews.R.id.keypad_6 -> addNumber(keyArray[6])
+                foundation.bluewhale.splashviews.R.id.keypad_7 -> addNumber(keyArray[7])
+                foundation.bluewhale.splashviews.R.id.keypad_8 -> addNumber(keyArray[8])
+                foundation.bluewhale.splashviews.R.id.keypad_9 -> addNumber(keyArray[9])
+                foundation.bluewhale.splashviews.R.id.keypad_delete_all -> deleteAll()
+                foundation.bluewhale.splashviews.R.id.keypad_delete_one -> deleteOne()
             }
         }
 
-        keypad_0.setOnClickListener(clickListener)
-        keypad_1.setOnClickListener(clickListener)
-        keypad_2.setOnClickListener(clickListener)
-        keypad_3.setOnClickListener(clickListener)
-        keypad_4.setOnClickListener(clickListener)
-        keypad_5.setOnClickListener(clickListener)
-        keypad_6.setOnClickListener(clickListener)
-        keypad_7.setOnClickListener(clickListener)
-        keypad_8.setOnClickListener(clickListener)
-        keypad_9.setOnClickListener(clickListener)
-        keypad_delete_all.setOnClickListener(clickListener)
+        for (i in 0..10) {
+            keypadList[i].setOnClickListener(clickListener)
+        }
         keypad_delete_one.setOnClickListener(clickListener)
 
         _disposables!!.add(
             RxView.clicks(button_forgot)
                 .throttleFirst(1, TimeUnit.SECONDS)
-                .subscribe { empty ->
+                .subscribe {
                     if (passwordListener != null)
                         passwordListener!!.onPasswordForgotClicked()
                 })
@@ -167,56 +176,29 @@ class PasswordView : ConstraintLayout {
         setBackspace();*/
 
         shuffleArray()
-        keypad_0.setText("" + keyArray[0])
-        keypad_1.text = "" + keyArray[1]
-        keypad_2.setText("" + keyArray[2])
-        keypad_3.setText("" + keyArray[3])
-        keypad_4.setText("" + keyArray[4])
-        keypad_5.setText("" + keyArray[5])
-        keypad_6.setText("" + keyArray[6])
-        keypad_7.setText("" + keyArray[7])
-        keypad_8.setText("" + keyArray[8])
-        keypad_9.setText("" + keyArray[9])
-
-        refreshUnderlineUI()
+        refreshUnderlineUI(if (list != null) list!!.size else 0)
     }
 
-    internal fun refreshColors() {
+    private fun refreshColors() {
         tv_title.setTextColor(pwTextColor)
         button_forgot.setTextColor(pwTextColor)
         v_line_forgot.setBackgroundColor(pwTextColor)
 
-        keypad_1.setTextColor(pwTextColor)
-        keypad_2.setTextColor(pwTextColor)
-        keypad_3.setTextColor(pwTextColor)
-        keypad_4.setTextColor(pwTextColor)
-        keypad_5.setTextColor(pwTextColor)
-        keypad_6.setTextColor(pwTextColor)
-        keypad_7.setTextColor(pwTextColor)
-        keypad_8.setTextColor(pwTextColor)
-        keypad_9.setTextColor(pwTextColor)
-        keypad_0.setTextColor(pwTextColor)
+        for (i in 0..10) {
+            keypadList[i].setTextColor(pwTextColor)
+        }
 
         keypad_delete_all.setTextColor(pwTextColor)
         iv_delete_one.setImageColor(pwTextColor)
 
         tv_error.setTextColor(pwErrorTextColor)
 
-        v_pwd_1.setBackground(ColorCircleDrawable(pwCircleColor))
-        v_pwd_2.setBackground(ColorCircleDrawable(pwCircleColor))
-        v_pwd_3.setBackground(ColorCircleDrawable(pwCircleColor))
-        v_pwd_4.setBackground(ColorCircleDrawable(pwCircleColor))
-        v_pwd_5.setBackground(ColorCircleDrawable(pwCircleColor))
-        v_pwd_6.setBackground(ColorCircleDrawable(pwCircleColor))
+        for (i in 0..5) {
+            pwdList[i].background = ColorCircleDrawable(pwCircleColor)
+            lineList[i].setBackgroundColor(pwUnderlineColor)
+        }
 
-        v_line_1.setBackgroundColor(pwUnderlineColor)
-        v_line_2.setBackgroundColor(pwUnderlineColor)
-        v_line_3.setBackgroundColor(pwUnderlineColor)
-        v_line_4.setBackgroundColor(pwUnderlineColor)
-        v_line_5.setBackgroundColor(pwUnderlineColor)
-        v_line_6.setBackgroundColor(pwUnderlineColor)
-
-        print()
+        print(if (list != null) list!!.size else 0)
     }
 
     fun setVisibilityOfForgotButton(show: Boolean) {
@@ -226,13 +208,19 @@ class PasswordView : ConstraintLayout {
 
     internal var list: MutableList<Int>? = null
 
-    internal fun addNumber(num: Int) {
+    private fun addNumber(num: Int) {
         if (list == null)
             list = ArrayList()
         if (list!!.size < 6)
             list!!.add(num)
 
-        print()
+        if (list!!.size > 0) {
+            print(list!!.size)
+            pwdList[list!!.size - 1].startAnimation(pwdAnim[list!!.size - 1])
+            lineList[list!!.size - 1].startAnimation(pwdLineAnim[list!!.size - 1])
+//            pwdList[list!!.size - 1].alpha = 1f
+//            lineList[list!!.size - 1].alpha = 1f
+        }
 
         if (list!!.size == 6) {
             if (callback_completed != null)
@@ -246,57 +234,34 @@ class PasswordView : ConstraintLayout {
         }
     }
 
-    fun deleteAll() {
+    private fun deleteAll() {
         if (list != null && list!!.size > 0) {
             list!!.clear()
         }
 
-        print()
+        print(if (list != null) list!!.size else 0)
     }
 
-    internal fun deleteOne() {
+    private fun deleteOne() {
         if (list != null && list!!.size > 0) {
             list!!.removeAt(list!!.size - 1)
         }
 
-        print()
+        print(if (list != null) list!!.size else 0)
     }
 
-    fun removePassword() {
-        if (list != null) {
-            list!!.clear()
-            print()
+    private fun print(size: Int) {
+        for (i in 0..5) {
+            pwdList[i].alpha = if (size >= i + 1) 1f else 0f
         }
+
+        refreshUnderlineUI(size)
     }
 
-    internal fun print() {
-        var size: Int
-        if (list != null)
-            size = list!!.size
-        else
-            size = 0
-
-        v_pwd_1.alpha = if (size >= 1) 1f else 0f
-        v_pwd_2.alpha = if (size >= 2) 1f else 0f
-        v_pwd_3.alpha = if (size >= 3) 1f else 0f
-        v_pwd_4.alpha = if (size >= 4) 1f else 0f
-        v_pwd_5.alpha = if (size >= 5) 1f else 0f
-        v_pwd_6.alpha = if (size >= 6) 1f else 0f
-
-        refreshUnderlineUI()
-    }
-
-    fun refreshUnderlineUI() {
-        var size = 0
-        if (list != null)
-            size = list!!.size
-
-        v_line_1.alpha = if (size >= 1) 1f else .3f
-        v_line_2.alpha = if (size >= 2) 1f else .3f
-        v_line_3.alpha = if (size >= 3) 1f else .3f
-        v_line_4.alpha = if (size >= 4) 1f else .3f
-        v_line_5.alpha = if (size >= 5) 1f else .3f
-        v_line_6.alpha = if (size >= 6) 1f else .3f
+    private fun refreshUnderlineUI(size: Int) {
+        for (i in 0..5) {
+            lineList[i].alpha = if (size >= i + 1) 1f else 0f
+        }
     }
 
     fun setTitle(title: String) {
@@ -316,7 +281,7 @@ class PasswordView : ConstraintLayout {
         fun onCompleted(password: String)
     }
 
-    internal var passwordListener: PasswordListener? = null
+    private var passwordListener: PasswordListener? = null
 
     fun setPasswordListener(passwordListener: PasswordListener) {
         this.passwordListener = passwordListener
@@ -335,6 +300,10 @@ class PasswordView : ConstraintLayout {
             val tmpPosition = rndInt % tmpList.size
             keyArray.add(tmpList[tmpPosition])
             tmpList.removeAt(tmpPosition)
+        }
+
+        for (i in 0..9) {
+            keypadList[i].text = keyArray[i].toString()
         }
     }
 }
