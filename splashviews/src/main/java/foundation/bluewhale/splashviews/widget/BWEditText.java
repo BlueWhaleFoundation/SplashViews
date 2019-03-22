cf2c588032232b7f339a37b52307441d54de87d4
Sungjun Hong <sungjun.app@gmail.com>
2019-03-22 13:45:44 +0900

    Update 2.0.52

--- splashviews/src/main/java/foundation/bluewhale/splashviews/widget/BWEditText.java
+++ splashviews/src/main/java/foundation/bluewhale/splashviews/widget/BWEditText.java
@@ -228,16 +228,13 @@ public class BWEditText extends RelativeLayout {
         iiv_clear = view.findViewById(R.id.iiv_clear);
         iiv_clear.setBackground(new ColorCircleDrawable(clearButtonColor));
 
-        //button_clear.setOnClickListener(v -> et_text.setText(""));
-
-        button_clear.setOnClickListener(new View.OnClickListener(){
+        button_clear.setOnClickListener(new OnClickListener(){
             @Override
             public void onClick(View v) {
-                //if (et_text.getText().length() > 0){
-                    //et_text.setText("");
-                //}
+
             }
         });
+        //button_clear.setOnClickListener(v -> et_text.setText(""));
 
 
         tv_hint = view.findViewById(R.id.tv_hint);
